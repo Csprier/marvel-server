926eccc7f2cbfddd364f3f276cc3bfffb912cee0
Cam Prier <CSPrier@gmail.com>
2019-02-14 19:15:05 -0500

    Added jwt auth passport authentication on line 29 in auth.js in routes

--- users/routes/auth.js
+++ users/routes/auth.js
@@ -8,18 +8,26 @@ const User = require('../models/user');
 const { JWT_SECRET, JWT_EXPIRY } = require('../../config');
 const router = express.Router();
 
-const localAuth = passport.authenticate('local', { session: false, failWithError: true });
+// Generate AuthToken for user
+const createAuthToken = (user) => {
+	return jwt.sign({ user }, JWT_SECRET, {
+		subject: user.username,
+		expiresIn: JWT_EXPIRY
+	});
+};
 
+const localAuth = passport.authenticate('local', { session: false, failWithError: true });
+router.use(bodyParser.json());
 // Login endpoint for login
 router.post('/login', localAuth, (req, res) => {
 	const authToken = createAuthToken(req.user);
 	return res.status(200).json({ authToken });
 });
 
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
 // Refresh AuthToken
 router.use('/refresh', passport.authenticate('jwt', { session: false, failWithError: true }));
-
-router.post('/refresh', (req, res, next) => {
+router.post('/refresh', jwtAuth, (req, res, next) => {
 	User.find({ id: req.user.id })
   .then(user => {
     const authToken = createAuthToken(user[0]);
@@ -30,13 +38,5 @@ router.post('/refresh', (req, res, next) => {
     next(err);
   });
 });
-
-// Generate AuthToken for user
-const createAuthToken = (user) => {
-	return jwt.sign({ user }, JWT_SECRET, {
-		subject: user.username,
-		expiresIn: JWT_EXPIRY
-	});
-};
 
 module.exports = router;
